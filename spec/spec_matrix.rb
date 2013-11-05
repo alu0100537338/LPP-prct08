9c844c0db9479a860fe0fb465620f39a67ee0d7c
Jessica Ramos <audiken_92@hotmail.com>
2013-11-05 16:09:24 +0000

    Definiendo tests operaciones

--- spec/spec_matrix.rb
+++ spec/spec_matrix.rb
@@ -6,9 +6,21 @@ describe Matriz do
 		
 	end
 	
-	describe "Pruebas metodos suma, resta y multiplicacion" do
+	describe "Pruebas metodos suma, resta, multiplicacion y multiplicacion por un numero" do
 		it "El metodo suma debe devolver el resultado de sumar dos matrices" do
 
+		end
+		
+		it "El metodo resta deve devolver el resultado de restar dos matrices" do
+		
+		end
+		
+		it "El metodo multiplicacion debe devolver el resultado de multiplicar dos matrices entre si" do
+		
+		end
+		
+		it "El metodo multin debe devolver el resuñtado de multiplicar una matriz por un numero" do
+		
 		end
 	end
 end
